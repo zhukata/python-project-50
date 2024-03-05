fe0e7daf5725849f267d2cff67c526b017561b99
zhukata <zhukata@gmail.com>
2024-03-05 20:23:23 +0300

    Package build. Step two

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+        )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
+    args = parser.parse_args()
+    print(args.first_file)
+    print(args.second_file)
+
+
+if __name__ == '__main__':
+    main()
